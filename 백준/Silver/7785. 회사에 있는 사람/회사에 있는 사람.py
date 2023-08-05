097af10685343fafe5f3cbfbffcf595247df31fc
moool01 <137693766+moool01@users.noreply.github.com>
2023-08-05 17:16:13 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 4292 ms, Memory: 48468 KB -BaekjoonHub

---
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.py
@@ -0,0 +1,17 @@
+n = int(input())
+temp = dict() # 딕셔너리 형
+
+for _ in range(n):
+    a, b = map(str, input().split())
+
+    if b == "enter":
+        temp[a] = b
+    # 퇴근을 했으면 삭제해준다.
+    elif b == "leave":
+        del temp[a]
+
+# 사전 순의 역순으로 정렬한다.
+temp = sorted(temp.keys(), reverse=True)
+
+for i in temp:
+    print(i)
