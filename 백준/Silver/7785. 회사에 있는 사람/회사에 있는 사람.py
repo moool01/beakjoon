9485faec1cb11ba90cd41f31d6a71205a615dfeb
moool01 <137693766+moool01@users.noreply.github.com>
2023-08-09 01:29:38 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 244 ms, Memory: 48468 KB -BaekjoonHub

--- 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.py
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.py
@@ -1,18 +1,16 @@
 import sys
 n = int(sys.stdin.readline())
-temp = dict() # 딕셔너리 형
+temp ={}
 
 for _ in range(n):
-    a, b = map(str, sys.stdin.readline().split())
+  a,b = map(str,sys.stdin.readline().split())
 
-    if b == "enter":
-        temp[a] = b
-    # 퇴근을 했으면 삭제해준다.
-    elif b == "leave":
-        del temp[a]
+  if b == "enter":
+    temp[a] = b
+  elif b == "leave":
+    del temp[a]
 
-# 사전 순의 역순으로 정렬한다.
-temp = sorted(temp.keys(), reverse=True)
+temp = sorted(temp.keys(),reverse=True)
 
 for i in temp:
-    print(i)
+  print(i)
